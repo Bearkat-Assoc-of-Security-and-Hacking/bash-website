c43844a520872f708007a953f8c8a7d41f65f568
TipsyPhiber <bam159@shsu.edu>
2025-07-01 11:29:03 -0500

    commit

---
+++ app/resources/page.js
@@ -0,0 +1,13 @@
+// app/resources/page.js
+export default function ResourcesPage() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold mb-4">Cyber Resources</h1>
+      <p className="mb-6">
+        Here is a curated list of useful websites, tools, and platforms for
+        learning cybersecurity.
+      </p>
+      {/* You will add your list of resources here later */}
+    </div>
+  );
+}
