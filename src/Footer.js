25d38736faa7c7860e0e80853ec64ec19446b880
TipsyPhiber <bam159@shsu.edu>
2025-07-08 11:02:28 -0500

    removed footer logo

--- src/Footer.js
+++ src/Footer.js
@@ -7,16 +7,6 @@ const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white p-4 mt-8">
       <div className="container mx-auto flex justify-between items-center">
-        {/* Logo */}
-        <Link href="/" className="hover:opacity-80 transition-opacity">
-          <Image
-            src="/bash-logo-transparent.png" // Your transparent logo
-            alt="BASH Logo"
-            width={120}
-            height={40}
-          />
-        </Link>
-
         {/* Copyright and License notice */}
         <div className="text-sm text-gray-400 text-right">
           <p>Copyright © {currentYear} BASH</p>
